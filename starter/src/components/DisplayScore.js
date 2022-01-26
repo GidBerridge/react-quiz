e26cee12a22112e44d7edd173377bf0a07c5d516
Gid Baerridge <gid.berridge@gmail.com>
2022-01-26 18:11:28 +0000

    it works

--- starter/src/components/DisplayScore.js
+++ starter/src/components/DisplayScore.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function DisplayScore(props) {
-  const scoreText = useState([
+  const scoreText = [
     'The man from Utopia!',
     'Wild love',
     "Tryin' To Grow A Chin",
@@ -9,30 +9,29 @@ function DisplayScore(props) {
     'Dancing fool',
     'Shut up and play your guitar',
     'Dumb all over',
-  ])
+  ]
+  const finalScore = props.totalScore()
 
   const scoreComment = () => {
-    // console.log(props.finalScore)
-    // console.log(props.qAmount)
-    if (props.finalScore === 10) {
+    if (finalScore === 10) {
       return scoreText[0]
     }
-    if (props.finalScore > 7 && props.finalScore < 10) {
+    if (finalScore > 7 && finalScore < 10) {
       return scoreText[1]
     }
-    if (props.finalScore > 5 && props.finalScore < 8) {
+    if (finalScore > 5 && finalScore < 8) {
       return scoreText[2]
     }
-    if (props.finalScore > 3 && props.finalScore < 6) {
+    if (finalScore > 3 && finalScore < 6) {
       return scoreText[3]
     }
-    if (props.finalScore > 1 && props.finalScore < 4) {
+    if (finalScore > 1 && finalScore < 4) {
       return scoreText[4]
     }
-    if (props.finalScore === 1) {
+    if (finalScore === 1) {
       return scoreText[5]
     }
-    if (props.finalScore === 0) {
+    if (finalScore === 0) {
       return scoreText[6]
     }
   }
@@ -40,9 +39,9 @@ function DisplayScore(props) {
   return (
     <div>
       <div className="score-section">
-        You scored {props.finalScore} out of {props.qAmount}
+        You scored {finalScore} out of {props.qAmount}
       </div>
-      <div className="score-section">{scoreComment(props.finalScore)}</div>
+      <div className="score-section">{scoreComment()}</div>
     </div>
   )
 }
