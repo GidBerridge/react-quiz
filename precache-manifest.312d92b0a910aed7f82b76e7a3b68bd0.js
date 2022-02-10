920030f01d054bd006c8856229b7e089f0c5155a
Gid Baerridge <gid.berridge@gmail.com>
2022-02-10 17:38:42 +0000

    Updates

---
+++ precache-manifest.312d92b0a910aed7f82b76e7a3b68bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c51a8fb8ba9ecd52123b08502ee99e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "631fec3fa08d0936ba40",
+    "url": "/static/css/main.5287509e.chunk.css"
+  },
+  {
+    "revision": "5dd0d1281eeeb6a78170",
+    "url": "/static/js/2.5cb2f71a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5cb2f71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631fec3fa08d0936ba40",
+    "url": "/static/js/main.8c623b5a.chunk.js"
+  },
+  {
+    "revision": "52ddb3d5bef29ae9d3a1",
+    "url": "/static/js/runtime-main.987dbaa4.js"
+  },
+  {
+    "revision": "b22a4a025c83bc1d1e52590b51180d09",
+    "url": "/static/media/zappa_bg_ol.b22a4a02.jpg"
+  }
+]);
