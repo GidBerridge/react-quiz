246fc256e6610aad6f81d60ce90e35f35e3b9401
Gid Baerridge <gid.berridge@gmail.com>
2022-09-12 11:34:44 +0100

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from "react"
 import QuestionBox from "../questionBox/QuestionBox"
-import './Header.scss' 
+import './Header.scss'
 import Subheading from "./Subheading"
 
 
 
 
 const Header = () => {
-    const [isActive, setIsActive] = useState(true)
+  const [isActive, setIsActive] = useState(true)
 
-    const handleShow = () => {
+  const handleShow = () => {
     setTimeout(() => {
       setIsActive(false)
     }, 3500)
-    
+
   }
 
-    return (
-        <>
-        <div className="header">
-            ZappaQuiz
-        </div>
-        {handleShow()}
-        {isActive ? <Subheading /> : <QuestionBox/>}
-        
-       
-        </>
-        
-    )
+  return (
+    <>
+      <div className="header">
+        Zappaquiz
+      </div>
+      {handleShow()}
+      {isActive ? <Subheading /> : <QuestionBox />}
+
+
+    </>
+
+  )
 }
 
 export default Header
