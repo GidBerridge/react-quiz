a7bd9293d93a684e6869ad46aaa204e5a0282da2
Gid Baerridge <gid.berridge@gmail.com>
2022-09-15 20:29:37 +0100

    all

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,40 +2,18 @@ import React from 'react'
 import './Header.scss'
 
 const Header = () => {
-<<<<<<< HEAD
-  const [isActive, setIsActive] = useState(true)
 
-  const handleShow = () => {
-    setTimeout(() => {
-      setIsActive(false)
-    }, 3500)
-
-  }
 
   return (
     <>
+
       <div className="header">
         ZappaQuiz
       </div>
-      {handleShow()}
-      {isActive ? <Subheading /> : <QuestionBox />}
-
 
     </>
-
   )
-=======
 
-    return (
-        <>
-
-          <div className="header">
-              ZappaQuiz
-          </div>
-
-        </>
-    )
->>>>>>> b467a1b4dfc10818bbe6988f7f8577a38ffc4c5a
 }
 
 export default Header
